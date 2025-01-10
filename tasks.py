07ca7ebaff28e977af376980b3facf5d8a4efd26
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-01-10 13:58:21 +0100

    changed tasks to include invoke dvc and others

--- tasks.py
+++ tasks.py
@@ -6,6 +6,43 @@ WINDOWS = os.name == "nt"
 PROJECT_NAME = "pokedec"
 PYTHON_VERSION = "3.12"
 
+
+@task
+def python(ctx):
+    """ """
+    ctx.run(
+        "which python" if os.name != "nt" else "where python",
+        echo=True,
+        pty=not WINDOWS,
+    )
+
+
+@task
+def git(ctx, message):
+    ctx.run(f"git add .")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
+
+
+@task
+def dvc(ctx, folder: str = "data", message: str = "Add_new_data"):
+    ctx.run(f"dvc add {folder}")
+    ctx.run(f"git add {folder}.dvc .gitignore")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
+    ctx.run(f"dvc push")
+
+
+@task
+def pull_data(ctx):
+    ctx.run("dvc pull")
+
+
+@task(pull_data)
+def train(ctx):
+    ctx.run("my_cli train")
+
+
 # Setup commands
 @task
 def create_environment(ctx: Context) -> None:
@@ -15,6 +52,7 @@ def create_environment(ctx: Context) -> None:
         echo=True,
         pty=not WINDOWS,
     )
+
 
 @task
 def requirements(ctx: Context) -> None:
@@ -29,16 +67,29 @@ def dev_requirements(ctx: Context) -> None:
     """Install development requirements."""
     ctx.run('pip install -e .["dev"]', echo=True, pty=not WINDOWS)
 
+
 # Project commands
 @task
 def preprocess_data(ctx: Context) -> None:
     """Preprocess data."""
-    ctx.run(f"python src/{PROJECT_NAME}/data.py data/raw data/processed", echo=True, pty=not WINDOWS)
+    ctx.run(
+        f"python src/{PROJECT_NAME}/data.py data/raw data/processed",
+        echo=True,
+        pty=not WINDOWS,
+    )
+
 
 @task
 def train(ctx: Context) -> None:
     """Train model."""
     ctx.run(f"python src/{PROJECT_NAME}/train.py", echo=True, pty=not WINDOWS)
+
+
+@task
+def evaluate(ctx: Context) -> None:
+    """Evaluate model."""
+    ctx.run(f"python src/{PROJECT_NAME}/evaluate.py", echo=True, pty=not WINDOWS)
+
 
 @task
 def test(ctx: Context) -> None:
@@ -46,25 +97,31 @@ def test(ctx: Context) -> None:
     ctx.run("coverage run -m pytest tests/", echo=True, pty=not WINDOWS)
     ctx.run("coverage report -m", echo=True, pty=not WINDOWS)
 
+
 @task
-def docker_build(ctx: Context, progress: str = "plain") -> None:
+def docker_build(ctx: Context) -> None:
     """Build docker images."""
     ctx.run(
-        f"docker build -t train:latest . -f dockerfiles/train.dockerfile --progress={progress}",
+        "docker build -t train:latest . -f dockerfiles/train.dockerfile",
         echo=True,
-        pty=not WINDOWS
+        pty=not WINDOWS,
     )
     ctx.run(
-        f"docker build -t api:latest . -f dockerfiles/api.dockerfile --progress={progress}",
+        "docker build -t api:latest . -f dockerfiles/api.dockerfile",
         echo=True,
-        pty=not WINDOWS
+        pty=not WINDOWS,
     )
+
 
 # Documentation commands
 @task(dev_requirements)
 def build_docs(ctx: Context) -> None:
     """Build documentation."""
-    ctx.run("mkdocs build --config-file docs/mkdocs.yaml --site-dir build", echo=True, pty=not WINDOWS)
+    ctx.run(
+        "mkdocs build --config-file docs/mkdocs.yaml --site-dir build",
+        echo=True,
+        pty=not WINDOWS,
+    )
 
 
 @task(dev_requirements)
