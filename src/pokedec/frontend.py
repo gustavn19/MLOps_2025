307ee48e4cb58592a5ac14e1ced87fae03fce445
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-01-22 15:35:46 +0100

    fixed names againg, underscore is not allowed in GCP

--- src/pokedec/frontend.py
+++ src/pokedec/frontend.py
@@ -14,7 +14,7 @@ def get_backend_url():
     client = run_v2.ServicesClient()
     services = client.list_services(parent=parent)
     for service in services:
-        if service.name.split("/")[-1] == "backend_pokedec":
+        if service.name.split("/")[-1] == "backend-pokedec":
             return service.uri
     name = os.environ.get("BACKEND", None)
     return name
